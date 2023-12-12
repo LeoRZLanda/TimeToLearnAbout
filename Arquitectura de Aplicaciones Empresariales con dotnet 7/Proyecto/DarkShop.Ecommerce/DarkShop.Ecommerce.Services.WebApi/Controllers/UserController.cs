a63a3e1610594cd669b1bb1881b5e81da78e5b1d
LeoRZLanda <leorzlanda@gmail.com>
2023-12-12 17:17:02 -0600

    vault backup: 2023-12-12 17:17:01 From:
    Affected files:
    Arquitectura de Aplicaciones Empresariales con dotnet 7/Proyecto/DarkShop.Ecommerce/DarkShop.Ecommerce.Services.WebApi/Controllers/UserController.cs

--- Arquitectura de Aplicaciones Empresariales con dotnet 7/Proyecto/DarkShop.Ecommerce/DarkShop.Ecommerce.Services.WebApi/Controllers/UserController.cs
+++ Arquitectura de Aplicaciones Empresariales con dotnet 7/Proyecto/DarkShop.Ecommerce/DarkShop.Ecommerce.Services.WebApi/Controllers/UserController.cs
@@ -4,12 +4,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DarkShop.Ecommerce.Application.DTO;
+using DarkShop.Ecommerce.Application.Interface;
+using DarkShop.Ecommerce.Services.WebApi.Helpers;
+using DarkShop.Ecommerce.Transversal.Common;
+using Microsoft.Extensions.Options;
 
 namespace DarkShop.Ecommerce.Services.WebApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly IUserApplication _userApplication;
+        private readonly AppSettings _appSettings;
+
+        public UserController(IUserApplication userApplication, IOptions<AppSettings> appSettings) {
+            _userApplication = userApplication;
+            _appSettings = appSettings.Value;
+        }
+
+        [HttpPost]
+        public IActionResult Authenticate([FromBody]UserDTO authDTO)
     }
 }
